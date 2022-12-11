dea9d37ccfd45c5f31b91821c31f4c3c856fa437
EnzeXu <xue20@wfu.edu>
2022-12-11 03:17:11 -0500

    updates

--- utils.h
+++ utils.h
@@ -14,6 +14,7 @@
 #define INF 99999999
 using namespace std;
 
+
 class Edge {
 public:
     int from, to;
@@ -29,10 +30,10 @@ public:
     int numNode;
     int numEdge;
     vector<Edge> edgeList;
-    d* map;
+    int* map;
     vector<vector<int> > neighborList;
     Graph(void) {};
-    Graph(int _numNode, int _numEdge, vector<Edge> _edgeList, d* _map): numNode(_numNode), numEdge(_numEdge), edgeList(_edgeList), map(_map) {
+    Graph(int _numNode, int _numEdge, vector<Edge> _edgeList, int* _map): numNode(_numNode), numEdge(_numEdge), edgeList(_edgeList), map(_map) {
         for (int i = 0; i < numNode; i++) {
             vector<int> v;
             for (int j = 0; j < numNode; j++) {
@@ -41,7 +42,9 @@ public:
             neighborList.push_back(v);
         }
     };
-    ~Graph(void) { printf("Graph destroyed.\n"); }
+    ~Graph(void) {
+        // printf("Graph destroyed.\n"); 
+    }
     void printEdges(void) {
         for (int i = 0; i < numEdge; i++) {
             edgeList[i].print();
@@ -61,7 +64,71 @@ public:
         for (int i = 0; i < numNode; i++) {
             printf("Node %d: ", i);
             for (int j = 0; j < numNode; j++) {
-                printf("%4.2lf ", map[i * numNode + j]);
+                printf("%d ", map[i * numNode + j]);
+            }
+            printf("\n");
+        }
+    }
+};
+
+class GraphLocal {
+public:
+    int numNodeAll;
+    int startNodeId;
+    int numNode;
+    int numEdge;
+    vector<Edge> edgeList;
+    int* map;
+    vector<vector<int> > neighborListLocal;
+    vector<vector<int> > neighborListGlobal;
+    GraphLocal(void) {};
+    GraphLocal(int _numNodeAll, int _startNodeId, int _numNode, int _numEdge, vector<Edge> _edgeList, int* _map): numNodeAll(_numNodeAll), startNodeId(_startNodeId), numNode(_numNode), numEdge(_numEdge), edgeList(_edgeList), map(_map) {
+        for (int i = 0; i < numNode; i++) {
+            vector<int> vLocal;
+            vector<int> vGlobal;
+            for (int j = 0; j < numNodeAll; j++) {
+                if (map[i * numNodeAll + j]) {
+                    vGlobal.push_back(j);
+                    if (j >= startNodeId && j < startNodeId + numNode) vLocal.push_back(j);
+                };
+            }
+            neighborListLocal.push_back(vLocal);
+            neighborListGlobal.push_back(vGlobal);
+        }
+    };
+    ~GraphLocal(void) {
+        // printf("Graph destroyed.\n");
+    }
+    void printEdges(void) {
+        for (int i = 0; i < numEdge; i++) {
+            edgeList[i].print();
+        }
+    }
+    void printNeighbors(void) {
+        // printf("Global Neighbors:\n");
+        for (int i = 0; i < numNode; i++) {
+            printf("Node %03d: [", i + startNodeId);
+            for (int j = 0; j < neighborListGlobal[i].size(); j++) {
+                printf("%d", neighborListGlobal[i][j]);
+                if (j != neighborListGlobal[i].size() - 1) printf(", ");
+            }
+            printf("]\n");
+        }
+        // printf("Local Neighbors:\n");
+        // for (int i = 0; i < numNode; i++) {
+        //     printf("Node %03d: [", i + startNodeId);
+        //     for (int j = 0; j < neighborListLocal[i].size(); j++) {
+        //         printf("%d", neighborListLocal[i][j]);
+        //         if (j != neighborListLocal[i].size() - 1) printf(", ");
+        //     }
+        //     printf("]\n");
+        // }
+    }
+    void printMap(void) {
+        for (int i = 0; i < numNode; i++) {
+            printf("Node %03d: ", i + startNodeId);
+            for (int j = 0; j < numNodeAll; j++) {
+                printf("%d ", map[i * numNodeAll + j]);
             }
             printf("\n");
         }
@@ -79,12 +146,38 @@ public:
     }
 };
 
+GraphLocal createRandomDirectedGraphLocal(int numNodeAll, int startNodeId, int numNode, int numEdge, int seed);
+
+
+GraphLocal createRandomDirectedGraphLocal(int numNodeAll, int startNodeId, int numNode, int numEdge, int seed) {
+    // printf("numNodeAll = %d numNode = %d startNodeId = %d numEdge = %d\n", numNodeAll, numNode,startNodeId, numEdge);
+    assert(numEdge <= numNode * (numNodeAll - 1));
+    srand(seed + startNodeId);
+    int* map = new int[numNode * numNodeAll];
+    for (int i = 0; i < numNode * numNodeAll; ++i) map[i] = 0;
+    // memset(map, 0, sizeof(map));
+    int numEdgeTarget = numEdge;
+    vector<Edge> edgeList;
+    while (numEdgeTarget > 0) {
+        int from = (rand() % numNode) + startNodeId;
+        int to = rand() % numNodeAll;
+        // printf("[%d->%d]\n", from, to);
+        if (from == to || map[(from - startNodeId) * numNodeAll + to] > 0) continue;
+        int weight = 1;
+        map[(from - startNodeId) * numNodeAll + to] = weight;
+        edgeList.push_back(Edge(from, to, weight));
+        numEdgeTarget --;
+    }
+    // printf("numNode = %d, numEdge = %d\n", numNode, numEdge);
+    return GraphLocal(numNodeAll, startNodeId, numNode, numEdge, edgeList, map);
+}
+
 Graph createRandomDirectedGraph(int numNode, int numEdge, int seed, d weight_max);
 
 Graph createRandomDirectedGraph(int numNode, int numEdge, int seed=0, d weight_max=10.0) {
     assert(numEdge <= numNode * (numNode - 1));
     srand(seed);
-    d* map = new d[numNode * numNode];
+    int* map = new int[numNode * numNode];
     memset(map, 0, sizeof(map));
     int numEdgeTarget = numEdge;
     vector<Edge> edgeList;
@@ -92,12 +185,7 @@ Graph createRandomDirectedGraph(int numNode, int numEdge, int seed=0, d weight_m
         int from = rand() % numNode;
         int to = rand() % numNode;
         if (from == to || map[from * numNode + to] > 0) continue;
-        d weight;
-        while (true) {
-            weight = rand() / (d) RAND_MAX * weight_max;
-            weight = d(int(weight));  // just test
-            if (weight > 0) break;
-        }
+        d weight = 1.0;
         map[from * numNode + to] = weight;
         edgeList.push_back(Edge(from, to, weight));
         numEdgeTarget --;
@@ -105,4 +193,5 @@ Graph createRandomDirectedGraph(int numNode, int numEdge, int seed=0, d weight_m
     // printf("numNode = %d, numEdge = %d\n", numNode, numEdge);
     return Graph(numNode, numEdge, edgeList, map);
 }
+
 
